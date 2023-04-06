540b8d10126411c6b911561910c83d214e3c55e4
alexp799 <80836235+alexp799@users.noreply.github.com>
2023-04-06 21:57:10 +0300

    solution

--- solution.py
+++ solution.py
@@ -1,17 +1,19 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
+from scipy.stats import chi2
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 753487228 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    stat = (x ** 2).sum()
+    
+    left_edge_chi_squared = chi2.ppf(alpha / 2, 2 * len(x)) 
+    right_edge_chi_squared = chi2.ppf(1 - alpha / 2, 2 * len(x))
+    
+    left_edge_sigma = np.sqrt(stat / (31 * right_edge_chi_squared))
+    right_edge_sigma = np.sqrt(stat / (31 * left_edge_chi_squared))
+    
+    return (left_edge_sigma, right_edge_sigma)
